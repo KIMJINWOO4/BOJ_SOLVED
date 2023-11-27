d9c6172f66a230384f51e7683037b8542219815a
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2023-11-27 19:57:51 +0900

    [Silver III] Title: N과 M (1), Time: 220 ms, Memory: 118220 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -0,0 +1,19 @@
+n, m = input().split()
+n = int(n)
+m = int(m)
+arr = []
+visit = [False] * (n+1)
+def dfs():
+    if len(arr) == m:
+        print(' '.join(map(str,arr)))
+        return
+    for i in range(1,n+1):
+        if visit[i]:
+            continue
+        visit[i] = True
+        arr.append(i)
+        dfs()
+        arr.pop()
+        visit[i] = False
+
+dfs()
