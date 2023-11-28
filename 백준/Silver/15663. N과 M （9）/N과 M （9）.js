69f2da92c8a5be0e0189e103673618cabfd29d61
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2023-11-28 13:13:19 +0900

    [Silver II] Title: N과 M (9), Time: 1340 ms, Memory: 17084 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.js
@@ -0,0 +1,30 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().trim().split('\n');
+
+let [n, m] = input[0].split(' ').map(Number);
+let numbers = input[1].split(' ').map(Number);
+let set = new Set();
+
+function backtrack(selected, visited) {
+    if (selected.length === m) {
+        set.add(selected.join(' '));
+        return;
+    }
+
+    for (let i = 0; i < n; i += 1) {
+        if (!visited[i]) {
+            visited[i] = true;
+            selected.push(numbers[i]);
+
+            backtrack(selected, visited);
+
+            selected.pop();
+            visited[i] = false;
+        }
+    }
+}
+
+numbers.sort((a, b) => a - b);
+backtrack([], Array(n).fill(false));
+for (i of set)
+  console.log(i);
