16c806ec62d7703e02be1666f6ce241b3cd0a9a5
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2023-12-04 10:55:38 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 936 ms, Memory: 16932 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.js
@@ -4,35 +4,23 @@ let input = fs.readFileSync('/dev/stdin').toString().split('\n');
 let [n, k] = input[0].split(' ').map(Number);
 
 function findMinTime(N, K) {
-  if (N >= K) {
+  if (N >= K)
       return N - K;
-  }
-
-  const visited = new Array(100001).fill(false);
-  const queue = [];
+  let visited = new Array(100001).fill(false);
+  let queue = [];
   queue.push([N, 0]);
-
   while (queue.length > 0) {
       const [current, time] = queue.shift();
 
-      if (current === K) {
+      if (current === K)
           return time;
-      }
-
       visited[current] = true;
-
-      if (current * 2 <= 100000 && !visited[current * 2]) {
+      if (current * 2 <= 100000 && !visited[current * 2])
           queue.unshift([current * 2, time]);
-      }
-
-      if (current + 1 <= 100000 && !visited[current + 1]) {
+      if (current + 1 <= 100000 && !visited[current + 1])
           queue.push([current + 1, time + 1]);
-      }
-
-      if (current - 1 >= 0 && !visited[current - 1]) {
+      if (current - 1 >= 0 && !visited[current - 1])
           queue.push([current - 1, time + 1]);
-      }
   }
 }
-
 console.log(findMinTime(n, k));
