63a972ce1c7e594fb70538bc0394e2197f99d697
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2023-12-28 22:12:01 +0900

    [Platinum II] Title: N-Queen 2, Time: 0 ms, Memory: 1116 KB, Score: 500 point -BaekjoonHub

--- 백준/Platinum/21133. N－Queen 2/N－Queen 2.c
+++ 백준/Platinum/21133. N－Queen 2/N－Queen 2.c
@@ -1,33 +1,27 @@
 #include <stdio.h>
 
-void placeQueens(int n) {
-    if (n % 6 != 2 && n % 6 != 3) {
-        for (int i = 2; i <= n; i += 2) {
+void placeQueens(int N) {
+    if (N % 6 == 2) {
+        for (int i = 2; i <= N; i += 2)
             printf("%d\n", i);
-        }
-        for (int i = 1; i <= n; i += 2) {
+        printf("3\n1\n");
+        for (int i = 7; i <= N; i += 2)
             printf("%d\n", i);
-        }
-    } else {
-        for (int i = 2; i <= n; i += 2) {
-            if (n % 6 == 3 && i == 2) continue;
+        if (N != 5) printf("5\n");
+    }
+    else if (N % 6 == 3) {
+        for (int i = 4; i <= N; i += 2)
             printf("%d\n", i);
-        }
-        if (n % 6 == 2) {
-            printf("3\n1\n");
-            for (int i = 7; i <= n; i += 2) {
-                printf("%d\n", i);
-            }
-            printf("5\n");
-        } else {
-            for (int i = 5; i <= n; i += 2) {
-                printf("%d\n", i);
-            }
-            printf("1\n3\n");
-        }
-        if (n % 6 == 3) {
-            printf("2\n");
-        }
+        printf("2\n");
+        for (int i = 5; i <= N; i += 2)
+            printf("%d\n", i);
+        printf("1\n3\n");
+    }
+    else {
+        for (int i = 2; i <= N; i += 2)
+            printf("%d\n", i);
+        for (int i = 1; i <= N; i += 2)
+            printf("%d\n", i);
     }
 }
 
