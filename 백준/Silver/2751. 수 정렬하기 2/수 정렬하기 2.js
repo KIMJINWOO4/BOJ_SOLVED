4a3eb675cc36967375719d50f5aa4717deae9837
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2023-12-29 15:33:47 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 1336 ms, Memory: 227760 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.js
@@ -0,0 +1,9 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = [];
+for (let i = 1; i <= n; i += 1)
+  arr.push(Number(input[i]));
+arr.sort((a, b) => a - b);
+console.log(arr.join('\n'));
