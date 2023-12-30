b83660cce097bd268a22004bb2104673baf4b58c
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2023-12-30 17:51:41 +0900

    [Silver I] Title: 카드 구매하기 2, Time: 184 ms, Memory: 10332 KB -BaekjoonHub

---
+++ 백준/Silver/16194. 카드 구매하기 2/카드 구매하기 2.js
@@ -0,0 +1,14 @@
+let fs = require('fs');
+let input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+let n = Number(input[0]);
+let arr = input[1].split(' ').map(Number);
+let dp = new Array(n + 1).fill(Number.MAX_SAFE_INTEGER);
+dp[0] = 0;
+
+for (let i = 1; i <= n; i += 1) {
+  for (let j = 1; j <= i; j += 1)
+    dp[i] = Math.min(dp[i], dp[i - j] + arr[j - 1]);
+}
+
+console.log(dp[n]);
