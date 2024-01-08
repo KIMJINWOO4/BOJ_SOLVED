a53972e7211bf4e5979c99d87355f06694100831
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2024-01-08 14:53:28 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 116 ms, Memory: 9308 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.js
@@ -0,0 +1,8 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+console.log('       _.-;;-._');
+console.log('\'-..-\'|   ||   |');
+console.log('\'-..-\'|_.-;;-._|');
+console.log('\'-..-\'|   ||   |');
+console.log('\'-..-\'|_.-\'\'-._|');
