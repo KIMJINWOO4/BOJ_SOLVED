a4e14c7af101614e7d2ea4df2e5e73a5fd72cec7
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2024-01-24 21:00:31 +0900

    [Bronze V] Title: 콜센터, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.js
@@ -0,0 +1,13 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+console.log('     /~\\');
+console.log('    ( oo|');
+console.log('    _\\=/_');
+console.log('   /  _  \\');
+console.log('  //|/.\\|\\\\');
+console.log(' ||  \\ /  ||');
+console.log('============');
+console.log('|          |');
+console.log('|          |');
+console.log('|          |');
