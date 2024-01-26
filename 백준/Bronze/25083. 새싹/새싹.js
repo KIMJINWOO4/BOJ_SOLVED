8b50a32cf79e83326e144d56e3fcf8fae7af1aae
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2024-01-26 15:55:17 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 9312 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.js
@@ -0,0 +1,9 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+console.log('         ,r\'"7');
+console.log('r\`-_   ,\'  ,/');
+console.log(' \\. \". L_r\'');
+console.log('   `~\\/')
+console.log('      |');
+console.log('      |');
