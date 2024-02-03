01fa8bfe70cf43d1a6cf9d908705e440ce85aaaa
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2024-02-03 16:56:24 +0900

    [Bronze V] Title: 웰컴, Time: 116 ms, Memory: 9316 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.js
@@ -0,0 +1,6 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+console.log('.  .   .');
+console.log('|  | _ | _. _ ._ _  _');
+console.log('|/\\|(/.|(_.(_)[ | )(/.');
