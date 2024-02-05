4701fa8c68e90d9e0e44d01e8a002bee389fb143
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2024-02-05 17:28:46 +0900

    [Bronze V] Title: Rook, Time: 124 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+console.log('  ___  ___  ___');
+console.log('  | |__| |__| |');
+console.log('  |           |');
+console.log('   \\_________/');
+console.log('    \\_______/');
+console.log('     |     |');
+console.log('     |     |');
+console.log('     |     |');
+console.log('     |     |');
+console.log('     |_____|');
+console.log('  __/       \\__');
+console.log(' /             \\');
+console.log('/_______________\\');
