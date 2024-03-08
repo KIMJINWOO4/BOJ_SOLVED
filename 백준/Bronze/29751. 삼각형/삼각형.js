e5e7041b30724760dd128d99c2d42e52125c9842
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2024-03-08 22:03:11 +0900

    [Bronze V] Title: 삼각형, Time: 192 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Bronze/29751. 삼각형/삼각형.js
@@ -0,0 +1,7 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+
+let [n, m] = input[0].split(" ").map(Number);
+let res = n * m / 2;
+
+console.log(res.toFixed(1));
