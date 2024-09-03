07c2695d546c14553735c755f71e12f3d550059d
Jinwoo Kim <rlawlsdn177@pusan.ac.kr>
2024-09-03 17:58:31 +0900

    [level 3] Title: 정수 삼각형, Time: 4.26 ms, Memory: 41.2 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.js
@@ -0,0 +1,7 @@
+function solution(triangle) {
+    for(let i = triangle.length - 2; i >= 0; i -= 1){
+        for(let j = 0; j < triangle[i].length; j += 1)
+            triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+    }
+    return triangle[0][0];
+}
